dc8ca4477a54955f3639f8c32b29c33c7811ceab
Rohitsharma-ITG <rohitsharma1@itgeeks.com>
2025-03-11 13:21:09 +0530

    update api code with new api

--- app/routes/api.orders/route.js
+++ app/routes/api.orders/route.js
@@ -1,6 +1,7 @@
 import { json } from "@remix-run/node";
 import mongoose from "mongoose";
 import dotenv from "dotenv"
+import { request } from "http";
 dotenv.config();
 
 
@@ -14,6 +15,19 @@ export const loader = async () => {
   return json(data);
 };
 
+
+export const action = async ({request}) => {
+    if (request.method === 'POST') {
+        const {id,orderAction} = await request.json();
+        const partnerId = new mongoose.Types.ObjectId(id);
+        const database = mongoose.connection.useDb(process.env.DATABASE_NAME);
+        const data = await database.collection("ordereditinghistories").find({partnerId,orderAction}).toArray();
+        if (!data) {
+            return json({ message: 'No Data Found' }, { status: 404 });
+        }
+        return json(data);
+    }
+}
 
 
 
